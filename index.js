e5cb67042efb0f4b22b86ec6c973cadacb8c3dcc
MariusFe <60129686+MariusFe@users.noreply.github.com>
2021-03-08 20:49:13 +0100

    Updates

--- index.js
+++ index.js
@@ -17,12 +17,13 @@ for(const file of commandFiles){
 
 client.once('ready', () =>{
     console.log('Bot online !');
-    numberOfRole = 0;
 })
 
 
 
 client.on('message', message=>{
+
+
 
 	if(!message.content.startsWith(config.prefix) || message.author.bot) return;
 
@@ -54,10 +55,61 @@ client.on('message', message=>{
 
         case 'weather': client.commands.get('weather').execute(message, args, client); break;
 
-        case 'role': 
+        case 'sncf':
+            client.commands.get('sncf').execute(message, args, client);
+        break;
+
+        case 'bet':
+            if(Number.isInteger(parseInt(args[2]))){
+                if(!args[3] || args[3] == "all"){
+                    client.commands.get('bet').execute(message, args, client);
+                }
+                else{client.commands.get('invalidCommand').execute(message, args, client);}
+            }
+            else if(args[2] == "start"){client.commands.get('betStart').execute(message, args, client);}
+            else if(args[2] == "balance" || args[2] == "bal"){client.commands.get('balance').execute(message, args, client);}
+            else if(args[2] == "stats"){client.commands.get('betStats').execute(message, args, client);}
+            else if(args[2] == "give"){client.commands.get('betGive').execute(message, args, client);}
+            else{client.commands.get('invalidCommand').execute(message, args, client);}
+            break;
+
+        case 'price':
+            if(args.length > 3){
+                client.commands.get('invalidCommand').execute(message, args, client);
+            }
+            else{
+                client.commands.get('price').execute(message, args, client);
+            }
+            break;
+
+        case 'stock':
+            if(args[2] == "start"){client.commands.get('stockStart').execute(message, args, client);}
+            else if(args[2] == "buy"){
+                if(args[5]){client.commands.get('invalidCommand').execute(message, args, client);}
+                else{client.commands.get('stockBuy').execute(message, args, client);}
+            }
+            else if(args[2] == "sell"){
+                if(args[5]){client.commands.get('invalidCommand').execute(message, args, client);}
+                else{client.commands.get('stockSell').execute(message, args, client);}
+            }
+            else if(args[2] == "help"){
+                if(args[3]){client.commands.get('invalidCommand').execute(message, args, client);}
+                else{client.commands.get('stockHelp').execute(message, args, client);}
+            }
+
+            else if(args[2] == "bal" || args[2] == "balance"){
+                if(args[3]){client.commands.get('invalidCommand').execute(message, args, client);}
+                else{client.commands.get('stockBalance').execute(message, args, client);}
+            }
+
+            else {client.commands.get('invalidCommand').execute(message, args, client);}
+
+            break;
+
+        /*case 'role': 
             client.commands.get('roleUser').execute(message, args, client, numberOfRole); 
             numberOfRole++;
-        break;
+        break;*/
 
     	default:
     		client.commands.get('invalidCommand').execute(message, args, client);
@@ -73,4 +125,4 @@ client.on('messageReactionAdd',(reaction, user) => {})
 
 
 //Used to connect the bot, the token is stored in Heroku
-client.login(process.env.TOKEN);
+client.login(config.token);
